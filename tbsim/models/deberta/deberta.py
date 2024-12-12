512ca74abd9dc57b1028c01100f9052a90fde5f1
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2024-12-12 16:37:09 +1100

    Freeze Deberta params

--- tbsim/models/deberta/deberta.py
+++ tbsim/models/deberta/deberta.py
@@ -6,6 +6,8 @@ class DebertaWithSingleOutput(torch.nn.Module):
     def __init__(self):
         super(DebertaWithSingleOutput, self).__init__()
         self.deberta = DebertaModel.from_pretrained("microsoft/deberta-base")
+        for param in self.deberta.parameters():
+            param.requires_grad = False
         self.pooler = torch.nn.Linear(self.deberta.config.hidden_size, 64)  # Reduce to desired dimension
 
     def forward(self, input_ids, attention_mask):
