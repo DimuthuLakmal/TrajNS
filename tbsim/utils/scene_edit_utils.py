8e1102ed20d3f2d20acf081fe7f92119674a6411
Dimuthu Kariyawasan Jalath Thanthrige <dkariyawasan@spartan-gpgpu154.hpc.unimelb.edu.au>
2025-04-30 13:34:16 +1000

    Local

--- tbsim/utils/scene_edit_utils.py
+++ tbsim/utils/scene_edit_utils.py
@@ -95,7 +95,7 @@ def guided_rollout(
     while not done:
         timers.tic("step")
         with timers.timed("obs"):
-            obs = env.get_observation()
+            obs = env.get_observation2()
             if obs == None:
                 return None, None, None
         with timers.timed("to_torch"):
